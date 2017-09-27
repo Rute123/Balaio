aa9a58f35f8863fbc99cb52cd5647ea5f0d2f66e
Camila Simões Marques Wanderley <camiila_simoes@172-16-3-21.0000.ufpe.br.iosda.org>
2017-09-27 14:13:08 -0300

    projeto com storyboard em andamento (falta id visual)

--- Balaio/FirstViewController.swift
+++ Balaio/FirstViewController.swift
@@ -1,18 +1,28 @@
 //
 //  ViewController.swift
-//  Balaio
+//  SemNome
 //
-//  Created by Camila Simões Marques Wanderley on 9/26/17.
+//  Created by Camila Simões Marques Wanderley on 9/25/17.
 //  Copyright © 2017 Camila Simões Marques Wanderley. All rights reserved.
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController  {
+
+  // Outlets
+  
+  @IBOutlet weak var selectIcons: UICollectionView!
+  
+  
+  @IBOutlet weak var mapFirst: MKMapView!
+  
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
   }
 
   override func didReceiveMemoryWarning() {
@@ -21,5 +31,16 @@ class ViewController: UIViewController {
   }
 
 
-}
+  
+  
+  // Actions
+  @IBAction func addActivitieButton(_ sender: Any) {
+  }
+  
+  
+  @IBAction func reloadButton(_ sender: Any) {
+  }
+  
+  
+ }
 
---
+++ Balaio/SelectIconsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SelectIconsCollectionViewCell.swift
+//  SemNome
+//
+//  Created by Camila Simões Marques Wanderley on 9/26/17.
+//  Copyright © 2017 Camila Simões Marques Wanderley. All rights reserved.
+//
+
+import UIKit
+
+// Collection view dos ícones, pra o filtro de busca por atividades
+class SelectIconsCollectionViewCell: UICollectionViewCell {
+
+  @IBOutlet weak var IconImageFilter: UIImageView!
+  
+  
+}
