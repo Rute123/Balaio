e4e7ccfcf37f41d2b855570bb9fd5dcdab45595d
Camila Simões Marques Wanderley <camiila_simoes@172-16-3-21.0000.ufpe.br.iosda.org>
2017-09-29 16:53:55 -0300

    add activities class organizada

--- Balaio/AddActivitieViewController.swift
+++ Balaio/AddActivitieViewController.swift
@@ -29,6 +29,8 @@ class AddActivitieViewController: UIViewController, UITextFieldDelegate, MKMapVi
   
   @IBOutlet weak var popview: PopView!
   
+  @IBOutlet weak var escolhaDaTag: UIImageView!
+  
   @IBOutlet weak var quandoAcabaLabel: UILabel!
   
   @IBOutlet weak var quandoAcabaDataPicker: UIDatePicker!
@@ -41,6 +43,8 @@ class AddActivitieViewController: UIViewController, UITextFieldDelegate, MKMapVi
   var locationNow: CLLocation?
   
   var screenLoadFirst: Bool = true
+ 
+  
   
   // View Did Load ()
   override func viewDidLoad() {
@@ -65,30 +69,13 @@ class AddActivitieViewController: UIViewController, UITextFieldDelegate, MKMapVi
     self.navigationController?.isNavigationBarHidden = false
     popview.isHidden = true
   }
+
   
   //Esconde o teclado ao clicar em Return
   func textFieldShouldReturn(_ textField : UITextField) -> Bool {
     textField.resignFirstResponder()
     return true
   }
-
-  // Actions
-  
-  @IBAction func escolhaDaTag(_ sender: UIButton) {
-    popview.isHidden = false
-  }
-  
-  //POPUP
-  @IBAction func celebrar(_ sender: UIButton) {
-  }
-  
-  @IBAction func contemplar(_ sender: UIButton) {
-  }
-  @IBAction func colaborar(_ sender: UIButton) {
-  }
-  @IBAction func praticar(_ sender: UIButton) {
-  }
-  
   
   
   // Dá o zoom na localização do evento (a mesma do usuário, por enquanto)
@@ -115,40 +102,7 @@ class AddActivitieViewController: UIViewController, UITextFieldDelegate, MKMapVi
     
   }
 
-  
-  
-  
-  // ENVIAR - append nova atividade no array
-  @IBAction func enviarEventoProBancoDeDados(_ sender: UIButton) {
-    
-    let componentesDoPicker: DateComponents = Calendar.current.dateComponents([.hour,.minute], from: self.quandoAcabaDataPicker.date)
-    let horaQueAcaba = componentesDoPicker.hour!
-    let minutoQueAcaba = componentesDoPicker.minute!
-   // jeito de comparar comentado na função refreshPin()
- 
-    
-    let novoEvento = CulturalActivities()
-    
-    novoEvento.activitieName = nomeEvento.text!
-    novoEvento.shortComment = descricaoEvento.text!
-    novoEvento.tag = celebrarTag // precisa de uma lógica pra pegar a tag escolhida
-    novoEvento.endsAt = "Acaba de \(horaQueAcaba)h\(minutoQueAcaba)min"
-    novoEvento.location = (locationNow?.coordinate)!
 
-    }
-    
-    // Mostra a barra de navegação
-    override func viewWillAppear(_ animated: Bool) {
-        self.navigationController?.isNavigationBarHidden = false
-        popview.isHidden = true
-    }
-    
-    //Esconde o teclado ao clicar em Return
-    func textFieldShouldReturn(_ textField : UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-    
     // Actions
     
     @IBAction func escolhaDaTag(_ sender: UIButton) {
@@ -176,6 +130,27 @@ class AddActivitieViewController: UIViewController, UITextFieldDelegate, MKMapVi
     
     
     
-    bancoDeDados.append(novoEvento)
+    // ENVIAR - append nova atividade no array
+    @IBAction func enviarEventoProBancoDeDados(_ sender: UIButton) {
+        
+        let componentesDoPicker: DateComponents = Calendar.current.dateComponents([.hour,.minute], from: self.quandoAcabaDataPicker.date)
+        let horaQueAcaba = componentesDoPicker.hour!
+        let minutoQueAcaba = componentesDoPicker.minute!
+        // jeito de comparar comentado na função refreshPin()
+        
+        
+        let novoEvento = CulturalActivities()
+        
+        novoEvento.activitieName = nomeEvento.text!
+        novoEvento.shortComment = descricaoEvento.text!
+        novoEvento.tag = celebrarTag // precisa de uma lógica pra pegar a tag escolhida
+        novoEvento.endsAt = "Acaba de \(horaQueAcaba)h\(minutoQueAcaba)min"
+        novoEvento.location = (locationNow?.coordinate)!
+        
+        // o init que vai ser usado: (activitiesName: String, location: CLLocationCoordinate2D, endsAt: String, tag: Tag, shortComment: String)
+        
+        bancoDeDados.append(novoEvento)
+        
+    }
     
 }
